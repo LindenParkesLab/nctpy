fe7f99bb132d79fabdde300c01f85e4b45167ece
Linden Parkes <lindenparkes@gmail.com>
2021-05-26 17:31:18 -0400

    added matrix_normalization and get_p_val_string functions

--- src/network_control/utils.py
+++ src/network_control/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+from scipy.linalg import svd
 
 def rank_to_normal(data, c, n):
     # Standard quantile function
@@ -28,3 +29,36 @@ def rank_int(data, c=3.0 / 8):
         transformed = transformed.reshape(dims)
 
     return transformed
+
+
+def matrix_normalization(A, c=1):
+    '''
+
+    Args:
+        A: np.array (n_parcels, n_parcels)
+            adjacency matrix from structural connectome
+        c: int
+            normalization constant, default=1
+
+    Returns:
+        A_norm: np.array (n_parcels, n_parcels)
+            normalized adjacency matrix
+
+    '''
+    u, s, vt = svd(A)  # singluar value decomposition
+    A_norm = A / (c + s[0])  # Matrix normalization
+
+    return A_norm
+
+
+def get_p_val_string(p_val):
+    if p_val == 0.0:
+        p_str = "-log10($\mathit{:}$)>25".format('{p}')
+    elif p_val < 0.001:
+        p_str = '$\mathit{:}$ < 0.001'.format('{p}')
+    elif p_val >= 0.001 and p_val < 0.05:
+        p_str = '$\mathit{:}$ < 0.05'.format('{p}')
+    else:
+        p_str = "$\mathit{:}$ = {:.3f}".format('{p}', p_val)
+
+    return p_str
