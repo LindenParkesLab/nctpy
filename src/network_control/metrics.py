c38fca9426f8219324e40b5183947b04173c6c48
Linden Parkes <lindenparkes@gmail.com>
2022-10-19 09:21:10 -0400

    added continuous version of average controllability

--- src/network_control/metrics.py
+++ src/network_control/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.linalg import schur
-
+from network_control.energies import gramian
 
 def node_strength(A):
     """ Returns strength of the nodes of a network.
@@ -20,14 +20,16 @@ def node_strength(A):
     return s
 
 
-def ave_control(A_norm):
+def ave_control(A_norm, version=None):
     """ Returns values of AVERAGE CONTROLLABILITY for each node in a network, given the adjacency matrix for that
     network. Average controllability measures the ease by which input at that node can steer the system into many
-    easily-reachable states. Expects input to be a DISCRETE system
+    easily-reachable states.
 
     Args:
         A_norm: np.array (n_parcels, n_parcels)
             Normalized adjacency matrix from structural connectome (see matrix_normalization in utils for example)
+        version: str
+            options: 'continuous' or 'discrete'. default=None
 
     Returns:
         ac: np.array (n_parcels,)
@@ -39,13 +41,21 @@ def ave_control(A_norm):
                6:8414, 2015.
     """
 
-    T, U = schur(A_norm, 'real')  # Schur stability
-    midMat = np.multiply(U, U).transpose()
-    v = np.matrix(np.diag(T)).transpose()
-    N = A_norm.shape[0]
-    P = np.diag(1 - np.matmul(v, v.transpose()))
-    P = np.tile(P.reshape([N, 1]), (1, N))
-    ac = sum(np.divide(midMat, P))
+    if version == 'discrete':
+        T, U = schur(A_norm, 'real')  # Schur stability
+        midMat = np.multiply(U, U).transpose()
+        v = np.matrix(np.diag(T)).transpose()
+        N = A_norm.shape[0]
+        P = np.diag(1 - np.matmul(v, v.transpose()))
+        P = np.tile(P.reshape([N, 1]), (1, N))
+        ac = sum(np.divide(midMat, P))
+    elif version == 'continuous':
+        n = A_norm.shape[0]
+        G = gramian(A_norm, B=np.eye(n), T=1, version=version)
+        ac = G.diagonal()
+    elif version == None:
+        raise Exception("Time system not specified. "
+                        "Please nominate whether you normalized A for a continuous-time or a discrete-time system")
 
     return ac
 
