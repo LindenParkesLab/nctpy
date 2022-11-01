1b71349ce1b3489254accac5499defe3d23b0532
Linden Parkes <lindenparkes@gmail.com>
2022-11-01 14:10:22 -0400

    replace optimal_control and minimum_control with new get_control_inputs that includes xr

--- src/network_control/energies.py
+++ src/network_control/energies.py
@@ -1,235 +1,37 @@
 import numpy as np 
 import scipy as sp
+from scipy import sparse
 import scipy.integrate
 import scipy.linalg as la
 from scipy.linalg import eig
-from scipy.linalg import svd
 from numpy import matmul as mm
 from scipy.linalg import expm as expm
 from numpy import transpose as tp
-from numpy import concatenate as cat
-from tqdm import tqdm
 
-from network_control.utils import matrix_normalization
 
-def sim_state_eq(A, B, x0, U, version=None):
-    """This function caclulates the trajectory for the network given our model
-     if there are no constraints, and the target state is unknown, using the
-     control equation precess x(t+1) = Ax(t) + BU(t). x(t) is the state vector, A is
-     the adjacency matrix, U(t) is the time varying input as specified by the
-     user, and B selects the control set (stimulating electrodes). This code assumes a DISCRETE system
-    
+def get_control_inputs(A_norm, T, B, x0, xf, system, xr='zero', rho=1, S='identity'):
+    """
+
     Args:
-     A             : NxN state matrix (numpy array), where N is the number of nodes in your
-                   network (for example, a structural connectivity matrix 
-                   constructed from DTI). A should be stable to prevent
-                   uncontrolled trajectories.
-    
-     B             : NxN input matrix (numpy array), where N is the number of nodes. B
-                   selects where you want your input energy to be applied to.
-                   For example, if B is the Identity matrix, then input energy
-                   will be applied to all nodes in the network. If B is a
-                   matrix of zeros, but B(1,1) = 1. then energy will only be
-                   applied at the first node.
-     
-     x0            : Nx1 initial state (numpy array) of your system where N is the number of
-                   nodes. x0 MUST have N rows.
-    
-     U             : NxT matrix of input (numpy array), where N is the number of nodes
-                   and T is the number of
-                   time points. For example, if you want to simulate the
-                   trajectory resulting from stimulation, U could have
-                   log(StimFreq)*StimAmp*StimDur as every element. You can
-                   also enter U's that vary with time
+        A_norm: (NxN numpy array) Normalized structural connectivity matrix
+        T: (float) Time horizon: how long you want to control for. Too large will give large error,
+            too short will not give enough time for control
+        B: (NxN numpy array) Input matrix: selects which nodes to put input into. Define
+            so there is a 1 on the diagonal of elements you want to add input to, and 0 otherwise
+        x0: (Nx1, numpy array) Initial state
+        xf: (Nx1, numpy array) Target state
+        system: (str) Time system: options, 'continuous' or 'discrete'
+        xr: (Nx1, numpy array) Reference state
+        rho: (float) Mixing parameter: determines the extent to which x is constrained alongside u
+        S: (NxN numpy array) Constraint matrix: determines which nodes for which u (and x) will be constrained
 
-     version        :options: 'continuous' or 'discrete' (str). default=None
-                    string variable that determines whether A is a continuous-time system or a discrete-time
-                    system
-    
-      Returns:
-     x             : x is the NxT trajectory (numpy array) that results from simulating
-                   x(t+1) = Ax(t) + Bu(t) the equation with the parameters
-                   above.
-    
-     @author JStiso 
-     June 2017
-    """
-    # check inputs
-    if version == 'continuous':
-        print("Simulating for a continuous-time system")
-    elif version == 'discrete':
-        print("Simulating for a discrete-time system")
-    elif version == None:
-        raise Exception("Time system not specified. "
-                        "Please indicate whether you are simulating a continuous-time or a discrete-time system "
-                        "(see matrix_normalization for help).")
-
-    # state vectors to float if they're bools
-    if type(x0[0]) == np.bool_:
-        x0 = x0.astype(float)
-
-    # check dimensions of states
-    if x0.ndim == 1:
-        x0 = x0.reshape(-1, 1)
-
-    # Simulate trajectory
-    T = np.size(U, 1)
-    N = np.size(A, 0)
-
-    # initialize x
-    x = np.zeros((N, T))
-    xt = x0
-    if version == 'discrete':
-        for t in range(T):
-            x[:, t] = np.reshape(xt, N)  # annoying python 1d array thing
-            xt_1 = np.matmul(A, xt) + np.matmul(B, np.reshape(U[:, t], (N, 1)))  # state equation
-            xt = xt_1
-    elif version == 'continuous':
-        for t in range(T):
-            x[:, t] = np.reshape(xt, N)  # annoying python 1d array thing
-            dt = np.matmul(A, xt) + np.matmul(B, np.reshape(U[:, t], (N, 1)))  # state equation
-            xt = dt + xt
-
-    return x
-
-
-def optimal_input(A, T, B, x0, xf, rho, S):
-    """This is a python adaptation of matlab code originally written by Tomaso Menara and Jason Kim
-     compute optimal inputs/trajectories for a system to transition between two states
-     Fabio, Tommy September 2017. This code assumes a CONTINUOUS system
-
-     Args:
-     A: (NxN numpy array) Structural connectivity matrix
-     B: (NxN numpy array) Input matrix: selects which nodes to put input into. Define
-           so there is a 1 on the diagonal of elements you want to add input to, 
-           and 0 otherwise 
-     S: (NxN numpy array) Selects nodes whose distance you want to constrain, Define so
-           that there is a 1 on the diagonal of elements you want to
-           constrain, and a zero otherwise
-     T: (float) Time horizon: how long you want to control for. Too large will give
-           large error, too short will not give enough time for control
-    rho: (float) weights energy and distance constraints. Small rho leads to larger
-           energy
-    
-     Returns:
-     X_opt: (TxN numpy array) 
-          The optimal trajectory through state space
-     U_opt: (TxN numpy array) 
-          The optimal energy
-     n_err: (float) 
-          the error associated with this calculation. Errors will be larger when B is not identity, 
-              and when A is large. Large T and rho will also tend to increase the error
-    
-    -------------- Change Log -------------
-        JStiso April 2018
-          Changed S to be an input, rather than something defined internally
-        
-        Jason Kim January 2021
-          Changed the forward propagation of states to matrix exponential to
-          avoid reliance on MATLAB toolboxes. Also changed definition of expanded
-          input U to save time by avoiding having to resize the matrix.
-          Also changed the initialization of U_opt for the same reason.
-        
-        JStiso 2021
-            Translated to Python
-
-    """
-
-    n = np.shape(A)[1]
-
-    # state vectors to float if they're bools
-    if type(x0[0]) == np.bool_:
-        x0 = x0.astype(float)
-    if type(xf[0]) == np.bool_:
-        xf = xf.astype(float)
-
-    # check dimensions of states
-    if x0.ndim == 1:
-        x0 = x0.reshape(-1, 1)
-    if xf.ndim == 1:
-        xf = xf.reshape(-1, 1)
-
-    Sbar = np.eye(n) - S
-    np.shape(np.dot(-B, B.T)/(2*rho))
-
-    Atilde = np.concatenate((np.concatenate((A, np.dot(-B, B.T)/(2*rho)), axis=1),
-                            np.concatenate((-2*S, -A.T), axis=1)), axis=0)
-
-    M = sp.linalg.expm(Atilde*T)
-    M11 = M[0:n, 0:n]
-    M12 = M[0:n, n:]
-    M21 = M[n:, 0:n]
-    M22 = M[n:, n:]
-
-    N = np.linalg.solve(Atilde, (M-np.eye(np.shape(Atilde)[0])))
-    c = np.dot(np.dot(N, np.concatenate((np.zeros((n, n)), S), axis=0)), 2*xf)
-    c1 = c[0:n]
-    c2 = c[n:]
-
-    p0 = np.dot(np.linalg.pinv(np.concatenate((np.dot(S, M12), np.dot(Sbar, M22)), axis=0)),
-                        (-np.dot(np.concatenate((np.dot(S, M11), np.dot(Sbar, M21)), axis=0), x0) -
-                         np.concatenate((np.dot(S, c1), np.dot(Sbar, c2)), axis=0) +
-                         np.concatenate((np.dot(S, xf), np.zeros((n, 1))), axis=0)))
-    
-    n_err = np.linalg.norm(np.dot(np.concatenate((np.dot(S, M12), np.dot(Sbar, M22)), axis=0), p0) -
-                           (-np.dot(np.concatenate((np.dot(S,M11),np.dot(Sbar, M21)), axis=0), x0) -
-                            np.concatenate((np.dot(S, c1), np.dot(Sbar, c2)), axis=0) +
-                            np.concatenate((np.dot(S, xf), np.zeros((n, 1))), axis=0)))  # norm(error)
-
-    STEP = 0.001
-    t = np.arange(0, (T+STEP/2), STEP)
-
-    U = np.dot(np.ones((np.size(t), 1)), 2*xf.T)
-
-    # Discretize continuous-time input for convolution
-    Atilde_d = sp.linalg.expm(Atilde*STEP)
-    Btilde_d = np.linalg.solve(Atilde, np.dot((Atilde_d-np.eye(2*n)), np.concatenate((np.zeros((n, n)), S), axis=0)))
-
-    # Propagate forward discretized model
-    xp = np.zeros((2*n, np.size(t)))
-    xp[:, 0:1] = np.concatenate((x0, p0), axis=0)
-    for i in np.arange(1, np.size(t)):
-        xp[:, i] = np.dot(Atilde_d, xp[:, i-1]) + np.dot(Btilde_d, U[i-1, :].T)
-
-    xp = xp.T
-
-    U_opt = np.zeros((np.size(t), np.shape(B)[1]))
-    for i in range(np.size(t)):
-        U_opt[i, :] = -(1/(2*rho))*np.dot(B.T, xp[i, n:].T)
-
-    X_opt = xp[:, 0:n]
-    
-    return X_opt, U_opt, n_err
-
-def minimum_input(A, T, B, x0, xf):
-    """ This function computes the minimum input required to transition between two states
-     
-     This is a python adaptation of code originally written by Jason Kim
-     
-     Computes minimum control input for state transition.
-
-     This code assumes a CONTINUOUS system
-     Args:
-      A: numpy array (N x N)
-            System adjacency matrix 
-      B: numpy array (N x N)
-            Control input matrix 
-      x0: numpy array (N x t)
-             Initial state 
-      xf: numpy array (N x t)
-            Final state 
-      T: float (1 x 1) 
-           Control horizon 
-      
     Returns:
-      x: numpy array (N x t)
-            State Trajectory 
-      u: numpy array (N x t)
-           Control Input 
+        x: (t x N numpy array) State trajectory (neural activity)
+        u: (t x N numpy array) Control inputs
+        err: (1 x 2 numpy array) Numerical error
     """
 
-    # System Size
-    n = np.shape(A)[0]
+    n_nodes = A_norm.shape[0]
 
     # state vectors to float if they're bools
     if type(x0[0]) == np.bool_:
@@ -242,42 +44,106 @@ def minimum_input(A, T, B, x0, xf):
         x0 = x0.reshape(-1, 1)
     if xf.ndim == 1:
         xf = xf.reshape(-1, 1)
-    
-    # Compute Matrix Exponential
-    AT = np.concatenate((np.concatenate((A, -.5*(B.dot(B.T))), axis=1), 
-                         np.concatenate((np.zeros(np.shape(A)), -A.T), axis=1)), axis=0)
 
-    E = sp.linalg.expm(AT*T)
+    if xr == 'x0':
+        xr = x0
+    elif xr == 'xf':
+        xr = xf
+    elif xr == 'zero':
+        xr = np.zeros((n_nodes, 1))
 
-    # Compute Costate Initial Condition
-    E12 = E[0:n, n:]
-    E11 = E[0:n, 0:n]
-    p0 = la.solve(E12, xf - E11.dot(x0))
+    if S == 'identity':
+        S = np.eye(n_nodes)
 
-    # Compute Costate Initial Condition Error Induced by Inverse
-    n_err = np.linalg.norm(E12.dot(p0) - (xf - E11.dot(x0)))
+    if system == 'continuous':
+        # Set parameters
+        dt = 0.001
 
-    # Prepare Simulation
-    STEP = 0.001
-    t = np.arange(0, (T+STEP/2), STEP)
+        # Define joint state-costate matrix
+        M = np.concatenate((np.concatenate((A_norm, np.dot(-B, B.T) / (2 * rho)), axis=1),
+                            np.concatenate((-2 * S, -A_norm.T), axis=1)), axis=0)
 
-    v0 = np.concatenate((x0, p0), axis=0)  # Initial Condition
-    v = np.zeros((2*n, len(t)))  # Trajectory
-    Et = sp.linalg.expm(AT*STEP)
-    v[:,0] = v0.T
+        # Define constant vector due to cost deviation from reference state
+        c = np.concatenate((np.zeros((n_nodes, 1)), 2 * np.dot(S, xr)), axis=0)
+        c = np.linalg.solve(M, c)
 
-    # Simulate State and Costate Trajectories
-    for i in np.arange(1, len(t)):
-        v[:, i] = Et.dot(v[:, i-1])
+        # Compute matrix exponential and decompose into NxN blocks
+        E = sp.linalg.expm(M * T)
+        r = np.arange(n_nodes)
+        E11 = E[r, :][:, r]
+        E12 = E[r, :][:, r + n_nodes]
 
-    x = v[0:n, :]
-    u = -0.5*B.T.dot(v[np.arange(0, n)+n, :])
+        # Solve for initial costate as a function of initial and final states
+        l0 = np.linalg.solve(E12,
+                             (xf - np.dot(E11, x0) - np.dot(np.concatenate((E11 - np.eye(n_nodes), E12), axis=1), c)))
 
-    # transpose to be similar to opt_eng_cont
-    u = u.T
-    x = x.T
+        # Construct discretized matrices to numerically integrate
+        z = np.zeros((2 * n_nodes, int(np.round(T / dt) + 1)))
+        z[:, 0] = np.concatenate((x0, l0), axis=0).flatten()
+        I = np.eye(2 * n_nodes)
+        Ad = sp.linalg.expm(M * dt)
+        Bd = np.dot((Ad - I), c)
 
-    return x, u, n_err
+        # Simulate the state-costate trajectory
+        for i in np.arange(1, int(np.round(T / dt)) + 1):
+            z[:, i] = np.dot(Ad, z[:, i - 1]) + Bd.flatten()
+
+        # Extract state and input from the joint state-costate equation
+        x = z[r, :]
+        u = np.dot(-B.T, z[r + n_nodes, :]) / (2 * rho)
+
+        # Collect error
+        err_costate = np.linalg.norm(np.dot(E12, l0) -
+                                     (xf - np.dot(E11, x0) -
+                                      np.dot(np.concatenate((E11 - np.eye(n_nodes), E12), axis=1), c)))
+        err_xf = np.linalg.norm(x[:, -1] - xf)
+        err = [err_costate, err_xf]
+    elif system == 'discrete':
+        # Define joint state - costate matrix
+        C = np.dot(-B, B.T) / (2 * rho)
+        D = 2 * S
+        I = np.eye(n_nodes)
+        z = np.arange(n_nodes)
+
+        # Construct system matrix
+        M = np.zeros(((2 * T - 1) * n_nodes, (2 * T - 1) * n_nodes))
+
+        # Constraints for the state equations
+        for i in np.arange(T + 1):
+            M[np.ix_(z + (i - 1) * n_nodes, z + (T - 2 + i) * n_nodes)] = -C
+            if i != T:
+                M[np.ix_(z + (i - 1) * n_nodes, z + (i - 1) * n_nodes)] = I
+            if i != 0:
+                M[np.ix_(z + (i - 0) * n_nodes, z + (i - 1) * n_nodes)] = -A_norm
+
+        # Constraints for the costate equations
+        for i in np.arange(1, T):
+            M[np.ix_(z + (i - 1 + T) * n_nodes, z + (i - 1) * n_nodes)] = -D
+            M[np.ix_(z + (i - 1 + T) * n_nodes, z + (T - 2 + i) * n_nodes)] = I
+            M[np.ix_(z + (i - 1 + T) * n_nodes, z + (T - 1 + i) * n_nodes)] = -A_norm.T
+
+        # Construct boundary condition vector
+        b = np.concatenate((np.dot(A_norm, x0), np.zeros((n_nodes * (T - 2), 1)), -xf, np.zeros((n_nodes * (T - 1), 1))),
+                           axis=0) - \
+            np.concatenate((np.zeros((n_nodes * T, 1)), np.tile(2 * np.dot(S, xr), (T - 1, 1))), axis=0)
+
+        # Solve simultaneous state and costate equations
+        M = sparse.csc_matrix(M)
+        b = sparse.csc_matrix(b)
+        v = sparse.linalg.spsolve(M, b)
+        # v = np.linalg.solve(M, b)
+        V = v.reshape((n_nodes, int(len(v) / n_nodes)), order='F')
+        x = np.concatenate((x0, V[:, :T - 1], xf), axis=1)
+        u = np.dot(-B.T, V[:, T - 1:]) / (2 * rho)
+
+        # Collect error
+        v = sparse.csc_matrix(np.expand_dims(v, axis=1))
+        err_system = np.dot(M, v) - b
+        err_system = np.linalg.norm(err_system.todense())
+        err_traj = np.linalg.norm(x[:, 1:] - (np.dot(A_norm, x[:, 0:-1]) + np.dot(B, u)))
+        err = [err_system, err_traj]
+
+    return x.T, u.T, err
 
 
 def minimum_energy_fast(A, T, B, x0_mat, xf_mat):
