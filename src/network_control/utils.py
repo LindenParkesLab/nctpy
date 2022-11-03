cfd54108fd2c1fcac64b0a26b929cf2309c662f1
Linden Parkes <lindenparkes@gmail.com>
2022-11-03 15:15:17 -0400

    clean out rank to norm functions

--- src/network_control/utils.py
+++ src/network_control/utils.py
@@ -6,38 +6,6 @@ import scipy.linalg as la
 from scipy.linalg import svd
 from scipy.linalg import eig
 from statsmodels.stats import multitest
-from numpy import matmul as mm
-from scipy.linalg import expm as expm
-from numpy import transpose as tp
-
-
-def rank_to_normal(data, c, n):
-    # Standard quantile function
-    data = (data - c) / (n - 2 * c + 1)
-    return sp.stats.norm.ppf(data)
-
-
-def rank_int(data, c=3.0 / 8):
-    if data.ndim > 1:
-        do_reshape = True
-        dims = data.shape
-        data = data.flatten()
-    else:
-        do_reshape = False
-
-    # Set seed
-    np.random.seed(0)
-
-    # Get rank, ties are averaged
-    data = sp.stats.rankdata(data, method="average")
-
-    # Convert rank to normal distribution
-    transformed = rank_to_normal(data=data, c=c, n=len(data))
-
-    if do_reshape:
-        transformed = transformed.reshape(dims)
-
-    return transformed
 
 
 def matrix_normalization(A, system=None, c=1):
