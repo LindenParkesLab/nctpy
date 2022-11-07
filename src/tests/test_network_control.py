bbc26249f44fca5d52e32e3d647a6d5e8dfbce3c
jastiso <jeni.stiso@gmail.com>
2022-11-07 09:42:01 -0500

    added scaffold for unittests

---
+++ src/tests/test_network_control.py
@@ -0,0 +1,53 @@
+import unittest
+
+
+# TODO test matrix_normalization
+class TestMatrixNormalization(unittest.TestCase):
+   def setUp(self):
+
+   def test_matrix_normalization_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+# TODO test normalize_state
+class TestNormalizeState(unittest.TestCase):
+   def setUp(self):
+
+   def test_normalize_state_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+# TODO test normalize_weights
+class TestNormalizeWeights(unittest.TestCase):
+   def setUp(self):
+
+   def test_normalize_weights_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+# TODO test get_control_inputs
+class TestGetControlInputs(unittest.TestCase):
+   def setUp(self):
+
+   def test_get_control_inputs_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+# TODO test integrate_u
+class TestIntegrateU(unittest.TestCase):
+   def setUp(self):
+
+   def test_integrate_u_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+# TODO test ave_control
+class TestAveControl(unittest.TestCase):
+   def setUp(self):
+
+   def test_ave_control_success(self):
+      self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+   unittest.main()
