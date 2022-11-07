8a7ab598dd3799bfc2e5030628605f0d0ab2dbc2
jastiso <jeni.stiso@gmail.com>
2022-11-07 11:34:18 -0500

    added tests for utils functions

---
+++ src/tests/make_fixtures.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy.linalg import eig
+import scipy as sp
+
+np.random.seed(1)
+# matrix norm
+N = 50
+b = np.random.randn(N, N)
+b_symm = (b + b.T)/2
+np.save('./fixtures/A.npy', b_symm)
+
+# eigenvalue decomposition
+A = b_symm
+w, _ = eig(A)
+l = np.abs(w).max()
+# Matrix normalization for discrete-time systems
+A_norm = A / (1 + l)
+np.save('./fixtures/A_d_1.npy', A_norm)
+A_norm = A_norm - np.eye(A.shape[0])
+np.save('./fixtures/A_c_1.npy', A_norm)
+
+# Matrix normalization for discrete-time systems
+A_norm = A / (2 + l)
+np.save('./fixtures/A_d_2.npy', A_norm)
+A_norm = A_norm - np.eye(A.shape[0])
+np.save('./fixtures/A_c_2.npy', A_norm)
+
+
+# state and weight norm
+x = np.random.randn(N, )
+np.save('./fixtures/x.npy', x)
+x_norm = x / np.linalg.norm(x, ord=2)
+np.save('./fixtures/x_norm.npy', x_norm)
+x_rank = sp.stats.rankdata(x)
+# rescale to unit interval
+x_scale = (x_rank - min(x_rank)) / (max(x_rank) - min(x_rank))
+np.save('./fixtures/weights_rank_scale.npy', x_scale)
+x_scale_const = x_scale + 1
+np.save('./fixtures/weights_rank_scale_const.npy', x_scale_const)
+x_scale = (x - min(x)) / (max(x) - min(x))
+np.save('./fixtures/weights_scale.npy', x_scale)
+x_scale_const = x_scale + 1
+np.save('./fixtures/weights_scale_const.npy', x_scale_const)
+
--- src/tests/test_network_control.py
+++ src/tests/test_network_control.py
@@ -1,52 +1,139 @@
 import unittest
+import sys
+import numpy as np
+# setting path
+sys.path.append('../network_control/')
+from network_control.utils import matrix_normalization, normalize_state, normalize_weights
+from network_control.metrics import ave_control
+from network_control.energies import integrate_u, get_control_inputs
 
 
-# TODO test matrix_normalization
 class TestMatrixNormalization(unittest.TestCase):
    def setUp(self):
+      with open('./fixtures/A.npy', 'rb') as f:
+         self.A = np.load(f)
 
    def test_matrix_normalization_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      # discrete time system default c=1
+      with open('./fixtures/A_d_1.npy', 'rb') as f:
+         A_d_1 = np.load(f)
+      A_d_1_test = matrix_normalization(self.A, system='discrete')
+      self.assertTrue((A_d_1 == A_d_1_test).all())
+      # discrete time system c=2
+      with open('./fixtures/A_d_2.npy', 'rb') as f:
+         A_d_2 = np.load(f)
+      self.assertTrue((A_d_2 == matrix_normalization(self.A, system='discrete', c=2)).all())
+      # continuous time system c=1
+      with open('./fixtures/A_c_1.npy', 'rb') as f:
+         A_c_1 = np.load(f)
+      self.assertTrue((A_c_1 == matrix_normalization(self.A, system='continuous', c=1)).all())
+      # continuous time system c=2
+      with open('./fixtures/A_c_2.npy', 'rb') as f:
+         A_c_2 = np.load(f)
+      self.assertTrue((A_c_2 == matrix_normalization(self.A, system='continuous', c=2)).all())
+
+   def test_system_specification_error(self):
+      # no default
+      with self.assertRaises(Exception) as exception_context:
+         matrix_normalization(self.A)
+      self.assertEqual(str(exception_context.exception),
+                       "Time system not specified. "
+                        "Please nominate whether you are normalizing A for a continuous-time or a discrete-time system "
+                        "(see function help).")
+
+      # typo
+      with self.assertRaises(Exception) as exception_context:
+         matrix_normalization(self.A, system="discrt")
+      self.assertEqual(str(exception_context.exception),
+                       "Time system not specified. "
+                       "Please nominate whether you are normalizing A for a continuous-time or a discrete-time system "
+                       "(see function help).")
+
+   # TODO add test for dimensions of A
 
 
-# TODO test normalize_state
 class TestNormalizeState(unittest.TestCase):
    def setUp(self):
+      with open('./fixtures/x.npy', 'rb') as f:
+         self.x = np.load(f)
 
    def test_normalize_state_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      with open('./fixtures/x_norm.npy', 'rb') as f:
+         x_norm = np.load(f)
+      self.assertTrue((x_norm == normalize_state(self.x)).all())
+
+       # TODO add type and size checking for inputs
 
 
-# TODO test normalize_weights
 class TestNormalizeWeights(unittest.TestCase):
    def setUp(self):
+      with open('./fixtures/x.npy', 'rb') as f:
+         self.weights = np.load(f)
 
    def test_normalize_weights_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      # defaults
+      with open('./fixtures/weights_rank_scale_const.npy', 'rb') as f:
+         weight_r_s_c = np.load(f)
+      self.assertTrue((weight_r_s_c == normalize_weights(self.weights)).all())
+      self.assertTrue((weight_r_s_c == normalize_weights(self.weights,
+                                                       rank=True,
+                                                       add_constant=True)).all())
+      # no rank
+      with open('./fixtures/weights_scale_const.npy', 'rb') as f:
+         weight_s_c = np.load(f)
+      self.assertTrue((weight_s_c == normalize_weights(self.weights,
+                                                       rank=False)).all())
+      # no constant
+      with open('./fixtures/weights_rank_scale.npy', 'rb') as f:
+         weight_r_s = np.load(f)
+      self.assertTrue((weight_r_s == normalize_weights(self.weights,
+                                                       add_constant=False)).all())
+      # no rank or constant
+      with open('./fixtures/weights_scale.npy', 'rb') as f:
+         weight_s = np.load(f)
+      self.assertTrue((weight_s == normalize_weights(self.weights,
+                                                     rank=False,
+                                                     add_constant=False)).all())
+
+      # TODO add type and size checking for inputs
 
 
-# TODO test get_control_inputs
 class TestGetControlInputs(unittest.TestCase):
    def setUp(self):
+      pass
 
    def test_get_control_inputs_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      # TODO defaults
+
+      #TODO T
+
+      #TODO B
+
+      #TODO rho
+
+      #TODO S
+
+      #TODO system
+      self.assertEqual(True, False)
+
+   def test_get_control_inputs_error(self):
+      #TODO system
+      self.assertEqual(True, False)
 
 
-# TODO test integrate_u
+# TODO pick tests integrate_u
 class TestIntegrateU(unittest.TestCase):
    def setUp(self):
+      pass
 
    def test_integrate_u_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      self.assertEqual(True, False)
 
 
-# TODO test ave_control
+# TODO pick tests ave_control
 class TestAveControl(unittest.TestCase):
-   def setUp(self):
-
    def test_ave_control_success(self):
-      self.assertEqual(True, False)  # add assertion here
+      self.assertEqual(True, False)
 
 
 if __name__ == '__main__':
